91c3ffcf4be7b2407ccb98107a655d02b88251fe
Kase1337 <linkinusf1@gmail.com>
2024-05-31 04:40:40 +0000

    Updated the comment

--- main.cs
+++ main.cs
@@ -8,7 +8,7 @@ class Program
     double Measurement_in_inches  = 2.54;
     int Input_value  = 15994;
     
-    // convert inches to centimeters aka multiply the entered value '5' by 2.54.
+    // convert inches to centimeters aka multiply the entered value '15994' by 2.54.
     double centimeters = Input_value  * Measurement_in_inches;
     
     // output the result of the conversion to the console.
