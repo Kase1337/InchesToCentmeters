b336044202ae7e077c1ca0279eb394dbd42aad99
Kase1337 <linkinusf1@gmail.com>
2024-05-31 04:22:57 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program 
+{
+  public static void Main (string[] args) 
+  {
+    //declare the required variables 
+    double Measurement_in_inches  = 2.54;
+    int Input_value  = 5;
+    
+    // convert inches to centimeters aka multiply the entered value '5' by 2.54.
+    double centimeters = Input_value  * Measurement_in_inches;
+    
+    // output the result of the conversion to the console.
+    Console.WriteLine((Input_value ) + " inches is "+ (centimeters)+ " centimeters");
+  }
+}
